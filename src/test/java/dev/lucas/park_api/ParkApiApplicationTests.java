4954d0c8fe08861e8296b23d64dc596d42a976ca
lucasrengel <lucas_rengel@estudante.sc.senai.br>
2024-09-07 12:32:23 -0300

    feat: inicia projeto

---
+++ src/test/java/dev/lucas/park_api/ParkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lucas.park_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
